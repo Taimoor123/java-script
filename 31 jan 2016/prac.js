38af095e808c09f58306f8455686a5209176b67a
Taimoor <taimoor_t@yahoo.com>
2016-01-31 14:40:37 +0500

    For loop and If condition work

---
+++ 31 jan 2016/prac.js
@@ -0,0 +1,18 @@
+for(var i=1;i<11;i++){
+  console.log(i*2);
+}
+
+
+for(var i=1;i<11;i++){
+  console.log(i + "   *  2  =  " + i*2);
+}
+
+
+
+
+
+for(var i=1;i<11;i++){
+  if(i%2==0){
+    console.log(i + "   *  2  =  " + i*2);
+  }
+}
